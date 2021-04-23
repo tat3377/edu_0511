9117f012fde108eee32746039084ef4326c3d8a7
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 18:10:13 +0300

    Create Main.java

---
+++ task_2.2/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i=10; i<11; i++ ) {
+            for (int j = 1; j<11;j++ ){
+                System.out.println(i+"*"+j+"="+i*j);
+        }
+
+
+}}}
