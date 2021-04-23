94cc7be27f6bd72221d502642c77c7e7c24dd1f1
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 18:06:43 +0300

    Create Main.java

---
+++ task_1.7/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c= scan.nextInt();
+        System.out.println(a * b * c * 1000);
+    }
+}
