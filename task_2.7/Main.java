628a77a1934ad5996ffd957bdc1f871d08a4a573
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 18:14:16 +0300

    Create Main.java

---
+++ task_2.7/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+      int a = 31;
+
+
+      for (int i = 2; i<a ; i++)
+           if (a%i == 0)
+                 System.out.println("true");
+           else System.out.println("false" );
+             }
+
+    }
