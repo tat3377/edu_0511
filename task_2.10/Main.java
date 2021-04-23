9c8d6599119b753f4ee4d358fa16ee4057312391
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 18:08:29 +0300

    Create Main.java

---
+++ task_2.10/Main.java
@@ -0,0 +1,8 @@
+  
+public class Main {
+    public static void main(String[] args) {
+      int [] a = {3,5,8,4,4,7,4};
+   for (int i=1; i<a.length; i++ )
+       if (a[i] == a [i-1]) System.out.println("Да");
+       else {System.out.println("Нет");
+}}}
