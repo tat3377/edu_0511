2fba016557205d3dd9f1ce2390edce44874e3daa
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 18:22:49 +0300

    Create Main.java

---
+++ task_3.5/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int num [] = {4,2,6,2,65};
+        for (int i = 0; i< num.length; i++) {
+            System.out.println(num [i]);
+        }
+        }
+
+    }
