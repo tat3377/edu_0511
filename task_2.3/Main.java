10f90c6b97a9ffc4a51dc40ed19641715c9ab334
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 18:10:58 +0300

    Create Main.java

---
+++ task_2.3/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        for(int a = 1 ; a <10; a++){
+            System.out.println("8");
+            for(int b=0 ; b < a; b++)
+                System.out.print("8");
+
+        }
+        System.out.println();//напишите тут ваш код
+
+    }
+}
