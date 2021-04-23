c7e4a30dcb7833df8d126ddd404f0a32b3260034
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 18:04:53 +0300

    Create Main.java

---
+++ task_1.5/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int tc = scan.nextInt();
+        int a = 9;
+        int b = 5;
+        int d = 32;
+        int div = a/b*tc+d ;
+
+        System.out.println(div +"*"+"/");
+
+
+    }
+}
