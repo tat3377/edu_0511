819e112c5309e5811a6703b2df3df3cd3872e09b
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 18:15:03 +0300

    Create Main.java

---
+++ task_2.8/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+      int[]numbers = {2,3,4,5};
+        int mult = 1;
+
+     for (int i = 0; i< 5; i++){
+        mult *= numbers[i];
+        System.out.println( mult );
+
+    }}}
