96c628ff98d84d74b77071a1af976b580a5a00c0
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 18:04:07 +0300

    Create Main.java

---
+++ task_1.4/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 25;
+        int mult = number*number;
+        System.out.println("Квадрат числа 25 равен "+mult);
+
+ }
+}
