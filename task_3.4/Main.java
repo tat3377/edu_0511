f8e1e8065001c4ca9298476da77055cc903805e8
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 18:21:32 +0300

    Create Main.java

---
+++ task_3.4/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+         if (a + b > 10) {
+            System.out.println(true);}
+            else
+            System.out.println(false);
+        }
+    }
