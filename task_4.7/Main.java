4678158e6f6b9a63d256809f93aabb8ef7dbd3f7
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 21:18:53 +0300

    Create Main.java

---
+++ task_4.7/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
+
+interface Animal {
+  void getColor();
+
+}
+abstract class Fox implements Animal{
+
+     String getName() {
+        return "Fox";
+    }
+
+}
