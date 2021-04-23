97a8510052d7d50d517588117130f03be8448d31
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 18:07:34 +0300

    Create Main.java

---
+++ task_2.1/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+       String a = "Мама";
+       String b = "Мыла";
+       String d = "Раму";
+        System.out.println(a+b+d);
+        System.out.println(b+a+d);
+        System.out.println(d+a+b);
+        System.out.println(d+b+a);
+        System.out.println(b+d+a);
+        System.out.println(a+d+b);
+    }
+}
