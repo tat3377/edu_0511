23c88bba02f5a5373f6406cf7b6cb4ba9f6d175e
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 18:13:28 +0300

    Create Main.java

---
+++ task_2.6/Main.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) throws Exception {
+      int a= 10;
+      int b= 3;
+
+           if (a%b == 0)
+                 System.out.println("Делится ");
+           else System.out.println("Делится с остатком:" + a%b);
+             }
+
+    }
