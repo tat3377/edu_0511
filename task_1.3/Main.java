7a669d0021286d786702fba80188b78ded6508dc
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 18:02:46 +0300

    Create Main.java

---
+++ task_1.3/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       int a = 12;
+       int b = 1916;
+       String date = "Дата";
+
+       System.out.print(date+ ":"+ a+ "." + b+"");
+
+    }
+}
