9ad6cb7411ca63ce2b3a9b2ac64d5013505e80cc
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 18:15:55 +0300

    Create Main.java

---
+++ task_2.9/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+      int[]numbers = {2,3,4,5};
+
+
+     for (int i = 0; i< 5; i++){
+
+        if (numbers [i]==5)
+        System.out.println("Да" );
+        else
+            System.out.println("Нет");
+    }}}
