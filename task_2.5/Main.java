a10f8e2fb6057d2336876e231e11d1be29858c3a
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 18:12:45 +0300

    Create Main.java

---
+++ task_2.5/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+
+           if (a == 10)
+                 System.out.println("Верно");
+           else System.out.println("Неверно");
+             }
+
+    }
