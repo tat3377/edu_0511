c6433d984f9b62b98e923ecb039bdc9c36c3ba75
tat3377 <48179901+tat3377@users.noreply.github.com>
2021-04-23 17:53:48 +0300

    Create Main.java

---
+++ task_1.2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String firstname = "Tatiana";
+        String city = "Moscow";
+        int age = 38;
+        System.out.println (firstname+" "+city+"-"+age+"");
+    }
+}
